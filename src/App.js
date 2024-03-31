d708a4f655988554bb20890384c24f2da540bfa8
Wilson <willythewhale8@gmail.com>
2024-03-31 13:56:40 -0400

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import Lottie from 'react-lottie-player';
+import coffeeLottie from "./Pre_comp_1.json";
+import scrollDown from "./scrollDown.json"
 function App() {
+  const [prevY, setPrevY] = useState(0);
+  const [scrollStatus, setScrollStatus] = useState(0);
+  const [isReversed, setIsReversed] = useState(false);
+  const [idle, setIdle] = useState(true)
+
+  useEffect(() => {
+    
+    const handleScroll = () => {
+      setIdle(false)
+      setTimeout(() => {
+        clearTimeout()
+        setIdle(true);
+      }, 5000);
+      
+      const currentY = window.scrollY;
+      if (prevY < currentY) {
+        if (scrollStatus >= 160) {
+            setScrollStatus(0);
+            setIsReversed(false); // Reset to forward play
+            return false;
+        }
+        setScrollStatus(scrollStatus + 0.55);
+
+      } else {
+        if (scrollStatus <= 0) { 
+            setScrollStatus(0);
+            setIsReversed(false); // Reset to forward play
+            return false;
+        }
+        setScrollStatus(scrollStatus - 0.55);
+      }
+
+      setPrevY(currentY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [prevY, scrollStatus]);
+
+
+  // Calculate the frame number 5 frames before the end of the animation
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='overLay'/>
+      {idle && <Lottie
+      className={`scrollSignal ${idle ? 'fadeIn' : ''}`}
+      animationData={scrollDown}
+      play
+      loop
+      />}
+      <div className="animation-container">
+        <Lottie 
+          className='animation'
+          animationData={coffeeLottie}
+          goTo={scrollStatus > 2 ? scrollStatus - 2 : 0} // Start from after frame 2
+          play={((scrollStatus > 50) || isReversed)}
+          loop= {isReversed}
+          direction={isReversed ? -1 : 1} // Reverse animation if isReversed is true
+          speed={isReversed ? 2 : 1} // Reverse animation if isReversed is true
+        />
+      </div>
     </div>
   );
 }
